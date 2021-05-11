75b4f0b525c272f2a97c1d6d9badaff95afbf084
wuwalaka <1980718207@qq.com>
2021-05-11 17:51:47 +0800

    添加了box

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
+    <box
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import Box from '../src/components/Box'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Box
   }
 }
 </script>
---
+++ src/components/Box.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.box{
+    width: 100px;
+    height: 100px;
+    background-color: aquamarine;
+}
+</style>
